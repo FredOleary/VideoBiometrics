faa5240e9569a3341c4a301daa8ff8f41d20845d
fredoleary <fred@essentel.com>
2019-09-26 15:32:33 -0700

    Make device foreign key required

--- server/models/heartrate.js
+++ server/models/heartrate.js
@@ -7,7 +7,8 @@ module.exports = (sequelize, DataTypes) => {
     greenFFT: {type: DataTypes.DOUBLE, allowNull:true},
     sumFFTs: {type: DataTypes.DOUBLE, allowNull:true},
     correlatedPkPk: {type: DataTypes.DOUBLE, allowNull:true},
-    correlatedFFTs: {type: DataTypes.DOUBLE, allowNull:true}
+    correlatedFFTs: {type: DataTypes.DOUBLE, allowNull:true},
+    DeviceId: {type: DataTypes.INTEGER, allowNull:false}
   }, {});
   HeartRate.associate = function(models) {
     HeartRate.belongsTo(models.Device, {
